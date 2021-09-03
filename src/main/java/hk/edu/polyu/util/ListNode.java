be21f61bd9b2c04784d9e2fec365593cba63b364
huangzuo <huangzuo@mi-ya.com.cn>
2021-09-03 14:24:14 +0800

    commit ListNode

--- src/main/java/hk/edu/polyu/util/ListNode.java
+++ src/main/java/hk/edu/polyu/util/ListNode.java
@@ -7,15 +7,9 @@ package hk.edu.polyu.util;
  * @e-mail hz920120@gmail.com
  */
 public class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {
-
-    }
-    ListNode(int val) {
-        this.val = val;
-    }
-    ListNode(int val, ListNode next) {
-        this.val = val; this.next = next;
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
+        this.val = x;
     }
 }
