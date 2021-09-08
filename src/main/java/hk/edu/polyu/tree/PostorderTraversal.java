bbeb0950f5bf7dfddc9a79a0d8dd0a8db9663abb
huangzuo <huangzuo@mi-ya.com.cn>
2021-09-08 13:04:41 +0800

    fix

--- src/main/java/hk/edu/polyu/tree/PostorderTraversal.java
+++ src/main/java/hk/edu/polyu/tree/PostorderTraversal.java
@@ -13,34 +13,52 @@ import java.util.Stack;
  * @e-mail hz920120@gmail.com
  */
 public class PostorderTraversal {
+//    public List<Integer> postorderTraversal(TreeNode root) {
+//        List<Integer> res = new ArrayList<>();
+//        if (root == null) {
+//            return res;
+//        }
+//
+//        Stack<TreeNode> stack = new Stack<>();
+//        Stack<TreeNode> collectStack = new Stack<>();
+//
+//        stack.push(root);
+//
+//        while (!stack.isEmpty()) {
+//            TreeNode treeNode = stack.pop();
+//            collectStack.push(treeNode);
+//
+//            if (treeNode.left != null) {
+//                stack.push(treeNode.left);
+//            }
+//
+//            if (treeNode.right != null) {
+//                stack.push(treeNode.right);
+//            }
+//        }
+//
+//        while (!collectStack.isEmpty()) {
+//            res.add(collectStack.pop().val);
+//        }
+//
+//        return res;
+//    }
+
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         if (root == null) {
             return res;
         }
-
-        Stack<TreeNode> stack = new Stack<>();
-        Stack<TreeNode> collectStack = new Stack<>();
-
-        stack.push(root);
-
-        while (!stack.isEmpty()) {
-            TreeNode treeNode = stack.pop();
-            collectStack.push(treeNode);
-
-            if (treeNode.left != null) {
-                stack.push(treeNode.left);
-            }
-
-            if (treeNode.right != null) {
-                stack.push(treeNode.right);
-            }
-        }
-
-        while (!collectStack.isEmpty()) {
-            res.add(collectStack.pop().val);
-        }
-
+        process(root, res);
         return res;
+    }
+
+    private void process(TreeNode treeNode, List<Integer> res) {
+        if (treeNode == null) {
+            return;
+        }
+        process(treeNode.left, res);
+        process(treeNode.right, res);
+        res.add(treeNode.val);
     }
 }
